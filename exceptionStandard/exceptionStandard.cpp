7fc15c5ed93033a6117e976ccefbf92b393af18d
taznimt <taznimanwar06@gmail.com>
2024-06-04 15:17:15 +0700

    exceptionStandard

---
+++ exceptionStandard/exceptionStandard.cpp
@@ -0,0 +1,27 @@
+
+
+#include <iostream>
+#include <exception>
+//untuk obyek exception yang akan digunakan
+#include <array>
+//untuk obyek array yag akan kita gunakan
+using namespace std;
+int main()
+{
+	cout << "Awal Program" << endl; //penanda 1:...
+	try {
+		array<int, 3> data = { 3, 5, 7 };
+		//pesan array integer 3 elemen
+		cout << data.at(5) << endl;
+		//memanggil array elemen ke 5
+	}
+	catch (exception& e) {
+		//penangkap menggunakan obyek exception
+		cout << e.what() << endl;
+		// akan dieksekusi karna array data hanya memiliki 3 elemen /
+	}
+	cout << "Baris Program Yang Terakhir" << endl;
+	// penanda 2: bahwa program berjalan tanpa henti meskipun terjadi kesalahan /
+		return 0;
+}
+
