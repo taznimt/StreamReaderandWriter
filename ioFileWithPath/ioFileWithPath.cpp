bd33b9b1fda3770594a67240e9658f1b78e3448d
taznimt <taznimanwar06@gmail.com>
2024-06-04 15:41:40 +0700

    memperbaiki codingan

--- ioFileWithPath/ioFileWithPath.cpp
+++ ioFileWithPath/ioFileWithPath.cpp
@@ -6,6 +6,8 @@ using namespace std;
 int main() {
     string baris;
     string NamaFile;
+    cout << "Masukan Nama File : ";
+    cin >> NamaFile;
 
     // membuka file dalam mode menulis.
     ofstream outfile;
