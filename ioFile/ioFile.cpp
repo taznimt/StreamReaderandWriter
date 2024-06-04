b812ce02fc523707645cab35d29234a585dff3e8
taznimt <taznimanwar06@gmail.com>
2024-06-04 14:47:34 +0700

    unlimited loop untuk menulis

--- ioFile/ioFile.cpp
+++ ioFile/ioFile.cpp
@@ -12,5 +12,9 @@ int main()
     //menunjuk ke sebuah nama file
     outfile.open("contohfile.txt");
     cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+    //unlimited loop untuk menulis
+    while (true) {
+
+    }
 }
 
