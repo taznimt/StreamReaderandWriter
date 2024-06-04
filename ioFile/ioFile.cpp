8e578994e64c32bd8295845a6c8c84de7c09378f
taznimt <taznimanwar06@gmail.com>
2024-06-04 15:02:27 +0700

    jika file ada maka melakukan perulangan setiap baris dan tampilkan

--- ioFile/ioFile.cpp
+++ ioFile/ioFile.cpp
@@ -30,5 +30,15 @@ int main()
     //menunjuk ke sebuah file
     infile.open("contohfile.txt");
     cout << endl << ">= Membuka dan membaca file " << endl;
+    // jika file ada maka
+    if (infile.is_open())
+    {
+        //melakukan perulangan setiap baris
+        while (getline(infile, baris))
+        {
+            //dan tampilkan disini
+            cout << baris << '\n';
+        }
+    }
 }
 
